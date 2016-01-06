5d2493bea3f796db179efd1f3cd7db8055d14459
AyushYadav <ayushyadav@outlook.com>
2016-01-06 21:15:38 +0530

    Added title extracting regex

--- script.py
+++ script.py
@@ -1,13 +1,15 @@
-import httplib2, re
+import re
 from urllib2 import Request, urlopen, URLError
 
-
 request = Request('http://www.thehindu.com/news/?service=rss')
-f=open("hindu_rssfeed.xml",'w')
-try:
-	response = urlopen(request)
-	news = response.read()
-	f.write(news)
-	print "grabbing completed"
-except URLError, e:
-    print 'Got an error code:', e
+f=open("hindu_rssfeed.xml",'r+')
+response = urlopen(request)
+news = response.read()
+f.write(news)
+f=f.read().replace("CDATA",'')
+print "grabbing completed"
+p = re.compile(ur'<title>(.+)<\/title>')
+test_str =f.read()
+retu=re.findall(p, test_str)
+print retu
+
