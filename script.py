05b0095d87879dc3f3ed19288c1c779bd94a76b5
AyushYadav <ayushyadav@outlook.com>
2016-01-06 21:19:10 +0530

    write error fixed

--- script.py
+++ script.py
@@ -2,13 +2,15 @@ import re
 from urllib2 import Request, urlopen, URLError
 
 request = Request('http://www.thehindu.com/news/?service=rss')
-f=open("hindu_rssfeed.xml",'r+')
+f=open("hindu_rssfeed.xml",'w')
 response = urlopen(request)
 news = response.read()
 f.write(news)
-f=f.read().replace("CDATA",'')
+#f=f.read().replace("CDATA",'')
 print "grabbing completed"
 p = re.compile(ur'<title>(.+)<\/title>')
+f.close()
+f=open("hindu_rssfeed.xml",'r')
 test_str =f.read()
 retu=re.findall(p, test_str)
 print retu
