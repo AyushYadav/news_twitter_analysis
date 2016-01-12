d2111007a53d2420727dbd8236d73da6d9d5a2bb
AyushYadav <ayushyadav@outlook.com>
2016-01-13 01:56:24 +0530

    automated get url and result

--- guardianfetch.py
+++ guardianfetch.py
@@ -8,26 +8,41 @@ f=open("theguardian_rssfeed.xml",'w')
 response = urlopen(request)
 news = response.read()
 f.write(news)
-print "grabbing completed"
+f.close()
+print "Grabbing completed"
 
 
-
+#Regex used for various tags
 title = re.compile(ur'<title>(.+)<\/title>')
 link = re.compile(ur'<link>(.+)<\/link>')
 desc = re.compile(ur'<description>(.+)<\/description>')
 pubDate = re.compile(ur'<pubDate>(.+)<\/pubDate>')
 
-f.close()
+
 f=open("theguardianuk.xml",'r').readlines()
-f=open('theguardian_rss.xml', 'w').writelines(f[18:-1])
+f=open('theguardian_rss.xml','w').writelines(f[18:-1]) #Removing the top content which is irrelevant
 f=open('theguardian_rss.xml','r')
+
 test_str =f.read()
 title_retu=re.findall(title, test_str)
 link_retu=re.findall(link, test_str)
 desc_retu=re.findall(desc, test_str)
 pubDate_retu=re.findall(pubDate, test_str)
 
-for i in title_retu:
-	print i
-	
+#Counting the no of news
+sz=len(title_retu)
+fo=open("refined_out_guardian.txt",'w') #Creating the output file
+for x in xrange(1,sz):
+	fo.write(title_retu[x])
+	fo.write("\n")
+	fo.write(pubDate_retu[x])
+	fo.write("\n")
+	fo.write(link_retu[x])
+	fo.write("\n")
+	fo.write(desc_retu[x])
+	fo.write("\n")
+	fo.write("\n")
+
+
+fo.close()
 
